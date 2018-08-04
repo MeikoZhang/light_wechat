c53375c9e5882cc1c02949981667495df208277c
MeikoZhang <krison_zhang@163.com>
2018-08-04 15:22:59 +0800

    2018080415

--- django_web/tests.py
+++ django_web/tests.py
@@ -3,9 +3,12 @@ from django.test import TestCase
 from django_web.models import WxRecord
 
 
+# 写完测试用例后，执行测试用例。这里与unittest的运行方法也不一样。
+# Django提供了“test”命令来运行测试，用cmd执行，测试插入的数据均在临时测试库中
+# python manage.py test
 class ModelTest(TestCase):
 
-    # 初始化：分别创建一条发布会（Event）和一条嘉宾（Guest）的数据。
+    # 初始化：创建一条数据
     def setUp(self):
         WxRecord.objects.create(msg_type='1', msg_time='2018-08-04 14:22:42',
                                 msg_from='凉城', msg_to='六岁就微信', msg_text='感觉')
@@ -21,7 +24,3 @@ class ModelTest(TestCase):
         for r in result:
             print("type:%s time:%s from:%-15s  to: %-15s  content:%s"
                   % (r.msg_type, r.msg_time, r.msg_from, r.msg_to, r.msg_text))
-
-    # 写完测试用例后，执行测试用例。这里与unittest的运行方法也不一样。
-    # Django提供了“test”命令来运行测试。（用cmd执行 见下截图）
-
